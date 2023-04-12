f1bc4a154208bccb2c5820f4f00ebae378b8ac45
anastasiiaEn <129371865+anastasiiaEn@users.noreply.github.com>
2023-04-12 20:59:38 +0300

    Add files via upload

---
+++ visualization/correlation_time_wind_speed.py
@@ -0,0 +1,18 @@
+# Code done by Yefremenko Anastasiia
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('/Users/anastasia/Desktop/programming python/data/weather/all_weather_by_hour.csv')
+
+# correlation between wind speed and time
+
+x = 'day_datetimeEpoch'
+y = 'day_windspeed'
+plot = df.plot(kind='scatter', x=x, y=y)
+
+plot.set_xlabel("Datetime Epoch")
+plot.set_ylabel("Wind speed")
+plot.set_title("Correlation between wind speed and time")
+
+plt.show()
