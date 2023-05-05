edd5de253a322e40759a4590c4027655bf88bd43
anastasiiaEn <129371865+anastasiiaEn@users.noreply.github.com>
2023-05-05 22:56:26 +0300

    Update correlation_time_solar_radiation.py

--- visualization/correlation_time_solar_radiation.py
+++ visualization/correlation_time_solar_radiation.py
@@ -3,12 +3,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# reads csv
 df = pd.read_csv('../data/weather/all_weather_by_hour.csv')
 
 x = 'day_datetimeEpoch'
 y = 'day_solarradiation'
+
+# draws plot
 plot = df.plot(kind='scatter', x=x, y=y)
 
+# names
 plot.set_xlabel("Datetime Epoch")
 plot.set_ylabel("Solar radiation")
 plot.set_title("Correlation between solar radiation in a day and time")
