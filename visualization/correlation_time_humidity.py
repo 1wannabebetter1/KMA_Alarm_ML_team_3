8f574b80a3df02bd0ce90c0da182ba3cd11f88fe
anastasiiaEn <129371865+anastasiiaEn@users.noreply.github.com>
2023-05-05 22:59:57 +0300

    Update correlation_time_humidity.py

--- visualization/correlation_time_humidity.py
+++ visualization/correlation_time_humidity.py
@@ -3,9 +3,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# reads csv
 df = pd.read_csv('../data/weather/all_weather_by_hour.csv')
 
-# correlation between datetime and humidity
+# plot correlation between datetime Epoch and humidity
 x = 'day_datetimeEpoch'
 y = 'day_humidity'
 plot = df.plot(kind='scatter', x=x, y=y)
