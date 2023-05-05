4be272c862afdeaae2e05164d7d763dc7834f42e
anastasiiaEn <129371865+anastasiiaEn@users.noreply.github.com>
2023-05-05 22:58:42 +0300

    Update correlation_time_max_temp.py

--- visualization/correlation_time_max_temp.py
+++ visualization/correlation_time_max_temp.py
@@ -3,13 +3,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# reads csv
 df = pd.read_csv('../data/weather/all_weather_by_hour.csv')
 
-# correlation with datetime and temperature
+# draws plot correlation with datetime Epoch and temperature
 x = 'day_datetimeEpoch'
 y = 'day_tempmax'
 plot = df.plot(kind='scatter', x=x, y=y)
 
+# names
 plot.set_xlabel("Datetime Epoch")
 plot.set_ylabel("Maximum temperature")
 plot.set_title("Correlation between maximum temperature and time")
