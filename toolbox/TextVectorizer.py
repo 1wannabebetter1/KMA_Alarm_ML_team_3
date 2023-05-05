61464f5600b32bade3d5f717b6ee6ffd6c3f87d5
anastasiiaEn <129371865+anastasiiaEn@users.noreply.github.com>
2023-05-05 22:51:35 +0300

    Update TextVectorizer.py

--- toolbox/TextVectorizer.py
+++ toolbox/TextVectorizer.py
@@ -1,12 +1,13 @@
+## function returns True if the input num
 def isNaN(num):
     return num != num
 
-
+## function takes a sparse matrix in coordinate format (coo_matrix) and sorts its elements by descending order of scores
 def sort_coo(coo_matrix):
     tuples = zip(coo_matrix.col, coo_matrix.data)
     return sorted(tuples, key=lambda x: (x[1], x[0]), reverse=True)
 
-
+## function extracts the top topn features with their corresponding scores and returns them as a dictionary
 def extract_topn_from_vector(feature_names, sorted_items, topn=10):
     # use only topn items from vector
     sorted_items = sorted_items[:topn]
@@ -27,7 +28,7 @@ def extract_topn_from_vector(feature_names, sorted_items, topn=10):
         results[feature_vals[idx]] = score_vals[idx]
     return results
 
-
+## function then extracts the top 100 words from the matrix using extract_topn_from_vector() and returns them as a dictionary.
 def conver_doc_to_vector(doc, cv, tfidf):
     feature_names = cv.get_feature_names_out()
     top_n = 100
